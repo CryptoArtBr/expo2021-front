f0855396ac69eeb8aabbdb5884c7722df1105b8e
amarocolas <amaroles@gmail.com>
2021-06-12 17:46:10 +0200

    add story pra bg com ilustra

---
+++ components/experiments/Illustras/BGChapado.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { BGChapado } from './BGChapado'
+
+export default {
+  title: 'Cabra/Experimentos/BGChapado',
+  component: BGChapado,
+}
+
+const Template = () => <BGChapado />
+
+export const Main = Template.bind({})
