bd26d27bde8a54cdcd5f8666c7dac8aa53e4c1ea
amarocolas <amaroles@gmail.com>
2021-06-15 23:39:18 +0200

    add signup form

---
+++ src/components/forms/SignUpForm/SignUpForm.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { Button, Input } from '../../atoms'
+import { initialData } from './data'
+
+export const SignUpForm = ({ onSubmit }) => {
+  const [formData, setFormData] = useState(initialData)
+
+  return (
+    <div>
+      <Input
+        label="nome"
+        value={formData.name}
+        placeholder="seu nome"
+        onChange={(e) =>
+          setFormData({
+            ...formData,
+            name: e.target.value,
+          })
+        }
+      />
+      <Input
+        label="email"
+        value={formData.email}
+        type="email"
+        placeholder="seu email"
+        onChange={(e) =>
+          setFormData({
+            ...formData,
+            email: e.target.value,
+          })
+        }
+      />
+      <Input
+        label="senha"
+        value={formData.password}
+        type="password"
+        placeholder="senha"
+        onChange={(e) =>
+          setFormData({
+            ...formData,
+            password: e.target.value,
+          })
+        }
+      />
+      <Button onClick={() => onSubmit(formData)}>Registrar</Button>
+    </div>
+  )
+}
---
+++ src/components/forms/SignUpForm/data.js
@@ -0,0 +1,5 @@
+export const initialData = {
+  name: '',
+  email: '',
+  password: '',
+}
